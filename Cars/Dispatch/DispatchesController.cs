6853840c299a4390315bcf34f2b8b1bf898498b3
CHL <a0972299435@gmail.com>
2025-09-30 17:59:10 +0800

    加上分頁

--- Cars/Dispatch/DispatchesController.cs
+++ Cars/Dispatch/DispatchesController.cs
@@ -1,14 +1,40 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Cars.Data;
+using Cars.ViewModels;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Cars.Dispatch
 {
+
     public class DispatchesController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public DispatchesController(ApplicationDbContext db) { _db = db; }
+
         [Authorize]
-        public IActionResult Dispatch()
+        public async Task<IActionResult> Dispatch(int page = 1, int pageSize = 10)
         {
-            return View();
+            var query = _db.CarApplications.AsNoTracking();
+
+            var total = await query.CountAsync();
+
+            var items = await query
+                .OrderByDescending(c => c.UseStart)
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            ViewBag.Pagination = new PaginationModel
+            {
+                CurrentPage = page,
+                PageSize = pageSize,
+                TotalItems = total,
+                Action = nameof(Dispatch),   // ← 分頁連到同一頁
+                Controller = "Dispatches"
+            };
+
+            return View(items); // ← 把這一頁的資料丟給 View
         }
         [Authorize]
         public IActionResult CarApply()
@@ -20,5 +46,8 @@ namespace Cars.Dispatch
         {
             return View();
         }
+       
+
+
     }
 }
---
+++ Cars/ViewModels/PaginationModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Cars.ViewModels
+{
+    public class PaginationModel
+    {
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages => (int)Math.Ceiling((double)TotalItems / Math.Max(1, PageSize));
+        public string? Action { get; set; }
+        public string? Controller { get; set; }
+        public RouteValueDictionary RouteValues { get; set; } = new();
+    }
+}
